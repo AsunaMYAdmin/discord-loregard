9be8bfd53acc18e397cae5a1fb1c9c0ffd4fbc9f
AsunaMYAdmin <kiritoadm@mail.ru>
2026-03-04 18:28:20 +0700

    Init commit

---
+++ src/main/java/me/asunamyadmin/discordloregardbot/DiscordLoregardBotApplication.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.discordloregardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordLoregardBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordLoregardBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/asunamyadmin/discordloregardbot/DiscordLoregardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.discordloregardbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordLoregardBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
